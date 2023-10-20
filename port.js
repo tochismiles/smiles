83302717797de22409da93197b1d6327b754b8fe
tochismiles <148525014+tochismiles@users.noreply.github.com>
2023-10-20 00:31:53 -0700

    Add files via upload

---
+++ port.js
@@ -0,0 +1,38 @@
+let revelation = document.getElementById("revealparty");
+let revealBtn = document.getElementById("revealbtn")
+const scroller = document.getElementsByClassName (".scroller")
+const nav = document.querySelector(".forlink")
+const ham = document.querySelector(".hambar")
+
+ham.addEventListener("click", () => {
+ nav.classList.add("forlink--open")
+console.log(nav.classList)
+})
+
+
+console.log(scroller)
+
+revealBtn.onclick = () => {
+    if(revelation.style.display == "none"){
+          revelation.style.display = "block"
+        }
+        else{
+          revelation.style.display = "none";
+        }
+   
+        let scrollerInside = document.getElementsByClassName("inner_scroller")
+        console.log(scrollerInside)
+        const scrollContent = Array.from(scrollerInside)
+        scrollContent.forEach(item =>{
+        let dubItem = item.cloneNode(true)
+            console.log(dubItem)
+            dubItem.setAttribute("aria-hidden", true)
+            // scrollerInside[0].appendChild(dubItem)
+        })
+  
+        // console.log(scrollContent)
+
+    
+
+}
+
